5c27144c8393a4ea5fb92b3f16ebc7e305e3c9b4
WarlockZul <warlockzul@gmail.com>
2026-03-15 18:47:01 +0800

    Added LSTM model and improved the GRU model by adding seed for reproduceability

--- scripts/deep_learning_4/train_lstm.py
+++ scripts/deep_learning_4/train_lstm.py
@@ -3,11 +3,11 @@ import random
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential # pyright: ignore[reportMissingModuleSource, reportMissingImports]
-from tensorflow.keras.layers import GRU, Dense, Dropout, BatchNormalization # pyright: ignore[reportMissingModuleSource, reportMissingImports]
-from tensorflow.keras.optimizers import Adam # pyright: ignore[reportMissingModuleSource, reportMissingImports]
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger # pyright: ignore[reportMissingModuleSource, reportMissingImports]
-from tensorflow.keras.regularizers import l2 # pyright: ignore[reportMissingModuleSource, reportMissingImports]
+from tensorflow.keras.models import Sequential # pyright: ignore[reportMissingModuleSource]
+from tensorflow.keras.layers import LSTM, Dense, Dropout, BatchNormalization # pyright: ignore[reportMissingModuleSource]
+from tensorflow.keras.optimizers import Adam # pyright: ignore[reportMissingModuleSource]
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger # pyright: ignore[reportMissingModuleSource]
+from tensorflow.keras.regularizers import l2 # pyright: ignore[reportMissingModuleSource]
 
 # Function to ensure deterministic/constant outputs for every run
 def set_seeds(seed_value=42):
@@ -37,25 +37,25 @@ def load_processed_data():
     
     return X_train, y_train, X_val, y_val
 
-# Build the GRU model
-def build_gru_model(input_shape):
+# Build the LSTM model
+def build_lstm_model(input_shape):
     """
-    Constructs the GRU model based on FYP Proposal:
-    - 2 GRU Layers
-    - 64 Hidden Units
+    Constructs the LSTM model based on FYP Proposal:
+    - 2 LSTM Layers
+    - 128 Hidden Units
     - Dropout 0.2
     """
     # L2 Regularization value
     reg_val = 0.00001
     
     model = Sequential([
-        # Layer 1: GRU
-        GRU(64, input_shape=input_shape, return_sequences=True),
+        # Layer 1: LSTM
+        LSTM(128, input_shape=input_shape, return_sequences=True),
         BatchNormalization(), 
-        Dropout(0.2),         
+        Dropout(0.2),        
         
-        # Layer 2: GRU
-        GRU(64, return_sequences=True),
+        # Layer 2: LSTM
+        LSTM(128, return_sequences=True),
         BatchNormalization(),
         Dropout(0.2),
         
@@ -63,7 +63,6 @@ def build_gru_model(input_shape):
         Dense(1, activation='linear', kernel_regularizer=l2(reg_val))
     ])
     
-    # Compile the model by defining optimizer, loss function, and metrics
     model.compile(
         optimizer=Adam(learning_rate=0.001),
         loss='mse',  
@@ -74,28 +73,23 @@ def build_gru_model(input_shape):
 
 # Main training function
 def main():
-    # Load data from processed files
     X_train, y_train, X_val, y_val = load_processed_data()
     
-    # Check input shape
-    # shape[1]: Time steps
-    # shape[2]: Features
     input_shape = (X_train.shape[1], X_train.shape[2]) 
     
-    # Build the GRU Model
-    model = build_gru_model(input_shape)
+    # Build the LSTM Model
+    model = build_lstm_model(input_shape)
     model.summary() 
     
-    # Set up Callbacks for Training 
-    os.makedirs("models/gru", exist_ok=True)
+    # Set up Callbacks for Training (Updated to lstm folder)
+    os.makedirs("models/lstm", exist_ok=True)
     callbacks = [
         EarlyStopping(monitor='val_loss', patience=8, restore_best_weights=True),
-        ModelCheckpoint("models/gru/gru_model.keras", monitor='val_mae', save_best_only=True),
-        CSVLogger("models/gru/gru_training_log.csv", separator=',', append=False)
+        ModelCheckpoint("models/lstm/lstm_model.keras", monitor='val_mae', save_best_only=True),
+        CSVLogger("models/lstm/lstm_training_log.csv", separator=',', append=False)
     ]
     
-    # Train the GRU model
-    print("\nStarting Training...")
+    print("\nStarting LSTM Training...")
     history = model.fit(
         X_train, y_train,
         validation_data=(X_val, y_val),
@@ -105,11 +99,9 @@ def main():
         verbose=1
     )
     
-    # Plot training history
     plot_training_history(history)
-    print("\nTraining Complete. Best model saved to 'models/gru_model.keras'")
+    print("\nTraining Complete. Best model saved to 'models/lstm/lstm_model.keras'")
 
-# Plot accuracy and loss over time (epochs)
 def plot_training_history(history):
     mae = history.history['mae']
     val_mae = history.history['val_mae']
@@ -119,19 +111,17 @@ def plot_training_history(history):
 
     plt.figure(figsize=(14, 5))
     
-    # Plot MAE of Loss (dB) vs Epochs
     plt.subplot(1, 2, 1)
     plt.plot(epochs, mae, 'bo-', label='Training MAE')
     plt.plot(epochs, val_mae, 'ro-', label='Validation MAE')
-    plt.title('Mean Absolute Error')
+    plt.title('LSTM: Mean Absolute Error')
     plt.ylabel('Error (dB)')
     plt.legend()
     
-    # Plot MSE of Loss (dB) vs Epochs
     plt.subplot(1, 2, 2)
     plt.plot(epochs, loss, 'bo-', label='Training Loss')
     plt.plot(epochs, val_loss, 'ro-', label='Validation Loss')
-    plt.title('Mean Squared Error (Loss)')
+    plt.title('LSTM: Mean Squared Error (Loss)')
     plt.legend()
     
     plt.tight_layout()
